6559592ee99f979fe5d637066a6b498d517d4178
YeEun_Choi <63354176+swcye@users.noreply.github.com>
2021-08-06 03:12:16 +0900

    Math

---
+++ BOJ/boj_1008.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class boj_1008 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double A=sc.nextDouble();
+        double B=sc.nextDouble();
+
+        double ans=A/B;
+        System.out.println(ans);
+    }
+}
